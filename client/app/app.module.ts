9af69c833f6c219b9005c15c264a0b6fb8a15bff
John Heinnickel` <jheinnic@gmail.com>
2017-03-17 20:51:29 -0700

    Root module imports

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -8,7 +8,8 @@ import {AsyncLocalStorageModule} from 'angular-async-local-storage';
 import {
   MdButtonModule, MdCardModule, MdIconModule, MdInputModule, MdTabsModule, MdToolbarModule,
   MdDialogModule, OverlayModule, PortalModule, MdChipsModule, MdSidenavModule, MdGridListModule,
-  MdTooltipModule, MdCheckboxModule, MdMenuModule
+  MdTooltipModule, MdCheckboxModule, MdMenuModule, MdButtonToggleModule, MdListModule,
+  MdSelectModule
 } from "@angular/material";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {Chance} from "chance";
@@ -28,18 +29,15 @@ import {SDKBrowserModule, ErrorHandler} from "./shared/sdk";
 import {CanvasCacheModule} from "./shared/canvas-cache";
 import {NewNamedCanvasModalComponent} from "./shared/phrase-generator/new-named-canvas-modal.component";
 import {NavBallsDirective} from "./app-root/navballs.directive";
-// import {CanvasAccessDirective} from "./shared/canvas-util/canvas-access.directive";
 import {PhraseGeneratorService} from "./shared/phrase-generator/phrase-generator.service";
 import {DIKeys} from "./shared/keys/keys.dictionary";
 import {GoogleLoginComponent} from "./authentication/google-login.component";
-import {ImageLobbySideComponent} from "./pool/image-lobby-side.component";
-import {ImageLobbyActionComponent} from "./pool/image-lobby-action.component";
-import {ScrollActionComponent} from "./scrolltest/scroll-action.component";
-import {ScrollSideComponent} from "./scrolltest/scroll-side.component";
-import {ScrollMainComponent} from "./scrolltest/scroll-main.component";
 import {ScrollModule} from "./scrolltest/scroll.module";
 import {CanvasAccessDirective} from "./shared/canvas-util/canvas-access.directive";
 import {PaintableDirective} from "./shared/canvas-util/paintable.directive";
+import {FlexSpacerDirective} from "./shared/blank-area/flex-spacer.directive";
+import {RealTimeManagerService} from "./shared/service-util/real-time-manager.service";
+import {MatchMediaService} from "./shared/service-util/match-media.service";
 
 function configureXSRFStrategy() {
   return new CookieXSRFStrategy('XSRF', 'X-XSRF-Token');
@@ -47,21 +45,9 @@ function configureXSRFStrategy() {
 
 @NgModule({
   declarations: [
-    CanvasAccessDirective,
-    NoContentComponent,
-    GoogleLoginComponent,
-    LoginModalComponent,
-    PassportComponent,
-    TopToolbarComponent,
-    InnerNavbarComponent,
-    FloatingActionsComponent,
-    NewNamedCanvasModalComponent,
-    NavBallsDirective,
-    // PointMapPipe,
-    // PaintablePointPipe,
-    // AllPointsComponent,
-    // AllPixelsComponent,
-    AppRootComponent
+    AppRootComponent, NoContentComponent, GoogleLoginComponent, LoginModalComponent,
+    PassportComponent, TopToolbarComponent, InnerNavbarComponent, FloatingActionsComponent,
+    NewNamedCanvasModalComponent, NavBallsDirective, CanvasAccessDirective, FlexSpacerDirective
   ],
   imports: [
     BrowserModule,
@@ -71,21 +57,24 @@ function configureXSRFStrategy() {
     AsyncLocalStorageModule,
     PoolModule,
     ScrollModule,
-    MdButtonModule.forRoot(),
-    MdCardModule.forRoot(),
-    MdCheckboxModule.forRoot(),
-    MdChipsModule.forRoot(),
-    MdDialogModule.forRoot(),
-    MdGridListModule.forRoot(),
-    MdIconModule.forRoot(),
-    MdInputModule.forRoot(),
-    MdMenuModule.forRoot(),
-    MdSidenavModule.forRoot(),
-    MdTabsModule.forRoot(),
-    MdToolbarModule.forRoot(),
-    MdTooltipModule.forRoot(),
-    OverlayModule.forRoot(),
-    PortalModule.forRoot(),
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdDialogModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+    OverlayModule,
+    PortalModule,
     NgbModule.forRoot(),
     CanvasCacheModule.forRoot(),
     RouterModule.forRoot(AppModule.routes),
@@ -101,27 +90,27 @@ function configureXSRFStrategy() {
       deps: []
     }, {
       provide: DIKeys.authHomeRoute,
-      useValue: '/home'
+      useValue: '/pools'
     }, {
       provide: DIKeys.apiUriHost,
       useValue: 'portfolio-api.dev.jchein.name:3000'
     }, {
       provide: DIKeys.googleLoginUrl,
       useValue: 'http://portfolio-api.dev.jchein.name:3000/auth/google'
-    }, NavbarDataService, PhraseGeneratorService, {
+    },
+    RealTimeManagerService, MatchMediaService, NavbarDataService, PhraseGeneratorService,
+    {
       provide: XSRFStrategy,
       useFactory: configureXSRFStrategy
     }
   ],
-  exports: [RouterModule, LoginModalComponent, NewNamedCanvasModalComponent, CanvasAccessDirective],
+  exports: [
+    RouterModule, LoginModalComponent, NewNamedCanvasModalComponent,
+    CanvasAccessDirective, FlexSpacerDirective
+  ],
   entryComponents: [
-    AppRootComponent,
-    LoginModalComponent,
-    GoogleLoginComponent,
-    PassportComponent,
-    NewNamedCanvasModalComponent,
-    InnerNavbarComponent,
-    TopToolbarComponent,
+    AppRootComponent, LoginModalComponent, GoogleLoginComponent, PassportComponent,
+    NewNamedCanvasModalComponent, InnerNavbarComponent, TopToolbarComponent,
     FloatingActionsComponent
   ],
   bootstrap: [AppRootComponent]
@@ -130,7 +119,7 @@ export class AppModule
 {
   static routes: Routes = [
     {
-      path: 'images',
+      path: 'pools',
       loadChildren: './pool/pool.module'
     }, {
       path: 'login',
@@ -151,24 +140,6 @@ export class AppModule
     }, {
       path: 'lobby',
       loadChildren: './+lobby/lobby.module#LobbyModule'
-    }, {
-      path: 'pixelMaps',
-      children: [ ]
-    //     {
-    //       path: '',
-    //       pathMatch: 'full',
-    //       component: AllPixelsComponent
-    //     }
-    //   ]
-    // }, {
-    //   path: 'pointMaps',
-    //   children: [
-    //     {
-    //       path: '',
-    //       pathMatch: 'full',
-    //       component: AllPointsComponent
-    //     }
-    //   ]
     }, {
       path: 'scroll',
       loadChildren: 'app/scrolltest/scroll.module'
