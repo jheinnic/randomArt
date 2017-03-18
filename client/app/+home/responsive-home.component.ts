6b8fdca402b9df69425a04d43819f5ee00aed68b
John Heinnickel` <jheinnic@gmail.com>
2017-03-17 20:52:04 -0700

    Responsive Utilities

--- client/app/+home/responsive-home.component.ts
+++ client/app/+home/responsive-home.component.ts
@@ -1,31 +1,29 @@
 /**
  * Created by jheinnic on 2/11/17.
  */
-import { Component, OnInit, NgZone } from '@angular/core';
-import { MatchMediaService } from '../shared/service-util/match-media.service';
-import { HomeMobileComponent } from './home-mobile.component';
-import { HomeDesktopComponent } from './home-desktop.component';
+import {Component, OnInit, NgZone} from "@angular/core";
+import {MatchMediaService} from "../shared/service-util/match-media.service";
+// import { HomeMobileComponent } from './home-mobile.component';
+// import { HomeDesktopComponent } from './home-desktop.component';
 
 @Component({
-  moduleId: module.id,
+  // moduleId: module.id,
   selector: 'home.component',
   templateUrl: 'home.component.html',
-  providers: [ MatchMediaService ],
-  directives: [ HomeMobileComponent, HomeDesktopComponent ]
+  providers: [MatchMediaService]
+  // directives: [ HomeMobileComponent, HomeDesktopComponent ]
 })
 export class ResponsiveHomeComponent implements OnInit
 {
-  IsMobile: Boolean = false;
-  IsDesktop: Boolean = false;
+  isMobile: Boolean = false;
+  isDesktop: Boolean = false;
 
   constructor(
-    private matchMediaService: MatchMediaService,
-    private zone: NgZone
-  )
-  {
+    private matchMediaService: MatchMediaService, private zone: NgZone
+  ) {
     //GET INITIAL VALUE BASED ON DEVICE WIDTHS AT TIME THE APP RENDERS
-    this.IsMobile = (this.matchMediaService.IsPhone() || this.matchMediaService.IsTablet());
-    this.IsDesktop = (this.matchMediaService.IsDesktop());
+    this.isMobile = (this.matchMediaService.isPhone() || this.matchMediaService.isTablet());
+    this.isDesktop = (this.matchMediaService.isDesktop());
 
     var that = this;
 
@@ -34,48 +32,34 @@ export class ResponsiveHomeComponent implements OnInit
      TAP INTO LISTENERS FOR WHEN DEVICE WIDTH CHANGES
      ---------------------------------------------------*/
 
-    this.matchMediaService.OnPhone(
-      function (mediaQueryList: MediaQueryList)
-      {
-        that.ShowMobile();
-      }
-    );
+    this.matchMediaService.onPhone(function (mediaQueryList: MediaQueryList) {
+      that.ShowMobile();
+    });
 
-    this.matchMediaService.OnTablet(
-      function (mediaQueryList: MediaQueryList)
-      {
-        that.ShowMobile();
-      }
-    );
+    this.matchMediaService.onTablet(function (mediaQueryList: MediaQueryList) {
+      that.ShowMobile(); // TODO: ShowMobile() or ShowDesktop() !?
+    });
 
-    this.matchMediaService.OnDesktop(
-      function (mediaQueryList: MediaQueryList)
-      {
-        that.ShowDesktop();
-      }
-    );
-  }
-
-  ngOnInit()
-  {
-
-  }
-
-  ShowMobile()
-  {
-    this.zone.run(() =>
-    { // Change the property within the zone, CD will run after
-      this.IsMobile = true;
-      this.IsDesktop = false;
+    this.matchMediaService.onDesktop(function (mediaQueryList: MediaQueryList) {
+      that.ShowDesktop()
     });
   }
 
-  ShowDesktop()
-  {
-    this.zone.run(() =>
-    { // Change the property within the zone, CD will run after
-      this.IsMobile = false;
-      this.IsDesktop = true;
+  ngOnInit() {
+
+  }
+
+  ShowMobile() {
+    this.zone.run(() => { // Change the property within the zone, CD will run after
+      this.isMobile = true;
+      this.isDesktop = false;
+    });
+  }
+
+  ShowDesktop() {
+    this.zone.run(() => { // Change the property within the zone, CD will run after
+      this.isMobile = false;
+      this.isDesktop = true;
     });
   }
 }
